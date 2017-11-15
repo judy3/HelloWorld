dd4ffe5cd5e8835d337fa7f8d25e7a2b75825dd3
judy3 <judy3.yang@gmail.com>
2017-11-15 15:35:19 +0800

    To decrease the execution time

--- aws_related/elb_ip.sh
+++ aws_related/elb_ip.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "USAGE: $0 load-balancer-name aws_profile"
-echo "OUTPUT FORMAT: EC2 ID :: IP"
-##This is used for: Get the instance IDs and IPs of an ELB
+echo "OUTPUT FORMAT: EC2 ID :: IP :: TYPE :: LIFECYCLE"
 
 PROFILE=${2:-"default"}
 ELB=$1
@@ -14,7 +13,10 @@ i=1
 while [ $i -le $EC2NUM ]
 do
   EC2_ID=$(sed -n $i'p' instanceid.txt)
-  INSTANCE_IP=$(aws --profile $PROFILE ec2 describe-instances --instance-ids $EC2_ID | jq -r '.Reservations[].Instances[].PublicIpAddress')
-  echo "$EC2_ID :: $INSTANCE_IP"
+  aws --profile $PROFILE ec2 describe-instances --instance-ids $EC2_ID > instance.json
+  INSTANCE_IP=$(cat instance.json | jq -r '.Reservations[].Instances[].PublicIpAddress')
+  EC2_TYPE=$(cat instance.json | jq -r '.Reservations[].Instances[].InstanceType')
+  EC2_TYPE2=$(cat instance.json | jq -r '.Reservations[].Instances[].InstanceLifecycle')
+  echo "$EC2_ID :: $INSTANCE_IP :: $EC2_TYPE :: $EC2_TYPE2"
 (( i++ ))
 done
