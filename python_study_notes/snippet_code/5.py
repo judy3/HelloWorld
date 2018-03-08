29965c89b66837fe69f42c63c5035f39b5ee4ce5
Yang Fei <yangfei@mail.jj.cn>
2018-03-08 10:13:55 +0800

    other easy python practice samples

--- python_study_notes/snippet_code/5.py
+++ python_study_notes/snippet_code/5.py
@@ -129,3 +129,238 @@ def oddOrEven():
         else:
             print("{0} is odd number.".format(x))
 #oddOrEven()
+
+def getmax():
+    s=input("please input a list of numbers, use comma to split:")
+    l=s.split(',')
+    m=max(l)
+    print("The max in {0} is {1}.".format(s,m))
+#getmax()
+
+def primeNumCheck():
+    a=int(input("input a number:"))
+    if a>1:
+        for c in range(2,a):   #range(2,2) no result
+            b=a%c
+            if b==0:
+                print("Not prime number.")
+                print("{0} can be devided by {1}.".format(a,c))
+                break
+        else:  ##如果for循环未被break终止，则执行else块中的语句或者for循环中不满足的条件执行else语句
+            print("yes, prime number.")
+    else:
+        print("not prime number")
+#primeNumCheck()
+
+def primeOut(a,b):
+    if a<1 or a==1:
+        a=2
+    c=b+1
+    l=[]
+    for d in range(a,c):
+        for e in range(a,d):
+            if d%e==0:
+                break
+        else:
+            l.append(d)
+    print(l)
+
+#primeOut(0,100)
+
+def factorial():
+    a=int(input("input a number:"))
+    b=a+1
+    d=1
+    if a==0:
+        d=1
+        print("factorial of {0} is {1}.".format(a,d))
+    elif a<0:
+        print("negetive number doesn't have factorial.")
+    else:
+        for i in range(1,b):
+            d=d*i
+        print("factorial of {0} is {1}.".format(a,d))
+#factorial()
+
+def showTimesTable():
+    for a in range(1,10):
+        for b in range(1,a+1):
+            c=a*b
+            print('{0}*{1}={2}\t'.format(a,b,c),end='')
+        print() #一定要有这一行
+#showTimesTable()
+
+# 1,2,3,4,5,6,7,8...
+# 0,1,1,2,3,5,8,13...
+def qiebo(n):
+    c1=0
+    c2=1
+    count=2
+    if n==1:
+        print("the list is: {}".format(c1))
+    elif n==0 or n<0:
+        print("no list")
+    else:
+        print("the list is {0} {1}".format(c1,c2),end=' ')
+        while count<n:
+            c=c1+c2
+            print(c,end=' ')
+            c1=c2
+            c2=c
+            count+=1
+#qiebo(50)
+
+#2
+def checkAmster(n):
+    length=len(str(n))
+    if n<0:
+        print("negetive number is not Amster")
+    else:
+        i=1
+        c=0
+        while i<length+1:
+            a=n%(10**i)//(10**(i-1))
+            b=a**length
+            d=c+b
+            i=i+1
+            c=d
+        if d==n:
+            print("a Amster")
+        else:
+            print("not a Amster")
+#checkAmster(153)
+
+def getAmster(min,max):
+    for n in range(min,max+1):
+        sum=0
+        length=len(str(n))
+        temp=n
+        while temp>0:
+            d=n%10
+            sum=d**length+sum
+            temp=temp//10
+        if sum==n:
+            print(n)
+#getAmster(0,500)
+
+def checkAmster2(num):
+    strnum=str(num)
+    length=len(strnum)
+    i=0
+    sum=0
+    while i<length:
+        sum=int(strnum[i])**length+sum
+        i=i+1
+    if sum==num:
+        print("{} is Amster".format(num))
+    else:
+        print("{} not Amster".format(num))
+#checkAmster2(153)
+
+def exchangeNum():
+    number=int(input("input a number:"))
+    print("decimal:{0}, binary:{1}, octonary:{2}, hexademical:{3}".format(number,bin(number),oct(number),hex(number)))
+#exchangeNum()
+
+def asiiToChr():
+    x=input("input a character:")
+    print("{0} ascii is: {1}".format(x,ord(x)))
+    y=int(input("input a ascii number:"))
+    print("{0} character is:{1}".format(y,chr(y)))
+#asiiToChr()
+
+def GCD(a,b):
+    if a>b:
+        smaller=b
+        bigger=a
+    else:
+        smaller=a
+        bigger=b
+    for i in range(1,smaller+1):
+        if smaller%i==0 and bigger%i==0:
+            gcd=i
+    print("the biggest common divisor for {0} and {1} is {2}.".format(a,b,gcd))
+    return int(gcd)
+#GCD(14,24)
+
+def lcm(a,b):
+    lcm=int(a*b/GCD(a,b))
+    print("least common multiple for {0} and {1} is {2}.".format(a,b,lcm))
+#lcm(4,6)
+
+def lcm2(a,b):
+    if a>b:
+        greater=a
+    else:
+        greater=b
+    while True:
+        if (greater%a==0) and (greater%b==0):
+             lcm=greater
+             break
+        greater+=1
+    return lcm
+#print(lcm2(4,6))
+
+def showmonth():
+    import calendar
+    year=int(input("input the year:"))
+    month=int(input("input the month:"))
+    print(calendar.month(year,month))
+#showmonth()
+
+def recur_fibo_number(n):
+    if n<=1:
+        return n
+    else:
+        return recur_fibo_number(n-2)+recur_fibo_number(n-1)
+def recur_fibo_list():
+    nterms=int(input("how many numbers you'd like to print out? "))
+    if nterms<=0:
+        print("must be postive.")
+    else:
+        for i in range(nterms):
+            print(recur_fibo_number(i),end=' ')
+#recur_fibo_list()
+
+def iofile():
+    with open("20180307.txt","wt") as outfile:
+        outfile.write("This line will be written to the file.")
+    with open("20180307.txt","rt") as infile:
+        out=infile.read()
+        print(out)
+#iofile()
+
+def checkStr():
+    str=input("input a string:")
+    print(str.isalnum())
+    print(str.isdigit())
+    print(str.isalpha())
+    print(str.islower())
+    print(str.isupper())
+    print(str.istitle())
+    print(str.isspace())
+#checkStr()
+
+def exchangeStr():
+    str=input("input a string:")
+    print(str.upper())
+    print(str.lower())
+    print(str.capitalize())
+    print(str.title())
+#exchangeStr()
+
+def daysOfMonth():
+    year=int(input("input the year:"))
+    month=int(input("input the month:"))
+    import calendar
+    print(calendar.monthrange(year,month))
+#daysOfMonth()
+
+def getDateBeforeToday():
+    import datetime
+    today=datetime.date.today()
+    duration=int(input("how many days ago of the date?"))
+    date1=datetime.timedelta(duration)
+    date2=today-date1
+    return date2
+print(getDateBeforeToday())
