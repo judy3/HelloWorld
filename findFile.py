8ccc1bb0615a953508f3451ebdee5db3e1035869
judy3 <judy3.yang@gmail.com>
2019-10-17 11:12:13 +0800

    Create findFile.py

---
+++ findFile.py
@@ -0,0 +1,15 @@
+import os
+
+'''
+编写一个程序，能在当前目录以及当前目录的所有子目录下查找文件名包含指定字符串的文件，并打印出相对路径。
+'''
+
+def findFile(str, path = '.'):
+    for x in os.listdir(path):
+        sub_path = os.path.join(path, x)
+        if os.path.isdir(sub_path):
+            findFile(str, sub_path)
+        if str in x and os.path.isfile(x):
+            print(sub_path)
+
+findFile('judy', '.')
